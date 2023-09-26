56e1ff640becc7e946d0c6488e199832fa9bb04e
qkrwogk <pjha999@naver.com>
2023-09-26 18:05:06 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Music Licensing Platform
+
+뮤지션들을 위한 포트폴리오 및 음원 판매 사이트입니다.
+
---
+++ src/main/java/qkrwogk/musiclicensing/MusicLicensingApplication.java
@@ -0,0 +1,13 @@
+package qkrwogk.musiclicensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLicensingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicLicensingApplication.class, args);
+    }
+
+}
